66d72432dd9144a33c1e14827b13b0927a4eda73
Jak <jaksawesome10@github.com>
2019-01-11 15:23:10 -0500

    stuff

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,23 @@
 
+function initiateStuff(){
+	
+	buttonTransform = document.getElementById("navButton").style.transform;
+	
+	if(buttonTransform == "") buttonTransform = "rotate(0)";
+	
+}
 
+function openNav(){
+	
+	buttonTransform = document.getElementById("navButton").style.transform;
+	
+	if(buttonTransform == "rotate(0)") buttonTransform = "rotate(90deg)";
+	else if(buttonTransform == "rotate(90deg)") buttonTransform = "rotate(180deg)";
+	else if(buttonTransform == "rotate(180deg)") buttonTransform = "rotate(270deg)";
+	else if(buttonTransform == "rotate(270deg)") buttonTransform = "rotate(0)";
+	
+	console.log(buttonTransform);
+	
+}
 
 
