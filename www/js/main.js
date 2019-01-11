e362f8c9d492853ca3950c9c6dd0466a40c8a78e
Jak <jaksawesome10@github.com>
2019-01-11 15:56:16 -0500

    headr finally works

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,13 @@
 
 function initiateStuff(){
-	
-	buttonTransform = document.getElementById("navButton").style.transform;
-	
-	if(buttonTransform == "") buttonTransform = "rotate(0)";
+
+	$('#navButton').click(function(){
+		
+		$(this).toggleClass('rotated');
+		
+		console.log("ok");
+		
+	});
 	
 }
-
-function openNav(){
-	
-	buttonTransform = document.getElementById("navButton").style.transform;
-	
-	if(buttonTransform == "rotate(0)") buttonTransform = "rotate(90deg)";
-	else if(buttonTransform == "rotate(90deg)") buttonTransform = "rotate(180deg)";
-	else if(buttonTransform == "rotate(180deg)") buttonTransform = "rotate(270deg)";
-	else if(buttonTransform == "rotate(270deg)") buttonTransform = "rotate(0)";
-	
-	console.log(buttonTransform);
-	
-}
-
 
