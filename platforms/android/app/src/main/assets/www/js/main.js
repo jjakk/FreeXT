4388a8121492c1ca78881d0aec48648500191503
Jak <jaksawesome10@github.com>
2019-01-12 14:29:19 -0500

    phonegap stuff

---
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -0,0 +1,20 @@
+
+function initiateStuff(){
+	
+	
+	
+}
+
+function openNav(){
+	
+	document.getElementById("navigation").className = "navOpen";
+	
+}
+
+function closeNav(){
+	
+	document.getElementById("navigation").className = "navClosed";
+	
+}
+
+
