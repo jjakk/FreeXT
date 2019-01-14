f6d44301943292acda564fc559336d7ba71caaed
Jak <jaksawesome10@github.com>
2019-01-13 20:24:48 -0500

    mobile stuff

--- platforms/windows/www/js/main.js
+++ platforms/windows/www/js/main.js
@@ -1,1 +1,26 @@
 ﻿
+function initiateStuff(){
+	
+	try{
+		
+		window.screen.lockOrientation("portrait");
+		
+	}
+	
+	catch{}
+	
+}
+
+function openNav(){
+	
+	document.getElementById("navigation").className = "navOpen";
+	
+}
+
+function closeNav(){
+	
+	document.getElementById("navigation").className = "navClosed";
+	
+}
+
+
