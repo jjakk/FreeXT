d7850bac39852dfccee67bb62d592dcdd208b263
Jak <jaksawesome10@github.com>
2019-02-12 21:10:26 -0500

    redesign stuff

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,6 +1,4 @@
 
-var inCordova = false;
-
 function sleep(seconds) {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -9,69 +7,17 @@ function sleep(seconds) {
   });
 }
 
-function initiateStuff(){
+window.addEventListener("orientationchange", function(){
 	
-	if(!!window.cordova){
-		
-        console.log("In cordova");
-		
-		inCordova = true;
-		
-    }
+	screen.orientation.lock('portrait');
 	
-   else{
-		
-		console.log("Not in cordova");
-		
-		inCordova = false;
-		
-	}
-	
-	if(inCordova){
-		
-		screen.orientation.lock('portrait');
-		
-	}
-	
-}
+});
 
-function openNav(){
+window.addEventListener("load", function initiateStuff(){
 	
-	document.getElementById("navigation").className = "navOpen";
 	
-}
+	
+});
 
-function closeNav(){
-	
-	document.getElementById("navigation").className = "navClosed";
-	
-}
-
-async function openSettings(){
-	
-	closeNav();
-	
-	if(window.matchMedia("(max-width: 600px)").matches){
-		
-		await sleep(500);
-		
-	}
-	
-	else{
-		
-		await sleep(1000);
-		
-	}
-	
-	
-	window.location = "software.html";
-	
-}
-
-function closeSettings(){
-	
-	window.location = "index.html";
-	
-}
 
 
