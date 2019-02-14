bc0d9322e44a2d399426f0b028e1318df3925ca1
Jak <jaksawesome10@github.com>
2019-02-14 17:37:38 -0500

    stuff

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -51,5 +51,42 @@ function closeNav(){
 	
 }
 
+function swipeStart(event){
+	
+	tapStartX = event.pageX;
+	
+	tapStartY = event.pageY;
+	
+}
+
+function swipeFinish(event){
+	
+	tapEndX = event.pageX;
+	
+	tapEndY = event.pageY;
+	
+	console.log(tapEndX)
+	
+	document.getElementById("mainNav").style.width = tapEndX;
+	
+}
+
+function touchSwipeStart(event){
+	
+	tapStartX = event.touches[0].clientX;
+	
+	tapStartY = event.touches[0].clientY;
+	
+}
+
+function touchSwipeEnd(event){
+	
+	tapEndX = event.changedTouches[0].clientX - tapStartX;
+	
+	tapEndY = event.changedTouches[0].clientY - tapStartY;
+	
+}
+
+
 
 
