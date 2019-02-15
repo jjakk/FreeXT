51e320dd25bf323480ee4ad9b70995446911213d
Jak <jaksawesome10@github.com>
2019-02-15 15:05:45 -0500

    stuff

---
+++ www/assets/js/index.js
@@ -0,0 +1,36 @@
+
+function openNav(){
+	
+	if(document.getElementById("mainNav").offsetWidth == 0){
+		
+		document.getElementById("mainNav").className = "navOpen";
+		
+		document.body.style.overflowY = "hidden";
+		
+		document.getElementById("sectionCover").style.display = "block";
+		
+	}
+	
+}
+
+async function closeNav(){
+	
+	if(document.getElementById("mainNav").offsetWidth != 0){
+		
+		document.getElementById("mainNav").className = "navClosed";
+		
+		document.body.style.overflowY = "auto";
+		
+		document.getElementById("sectionCover").style.display = "none";
+		
+	}
+	
+}
+
+function openSettings(){
+	
+	closeNav().then(location.href = "settings.html");
+	
+}
+
+
--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -19,34 +19,6 @@ window.addEventListener("load", function initiateStuff(){
 	
 });
 
-function openNav(){
-	
-	if(document.getElementById("mainNav").offsetWidth == 0){
-		
-		document.getElementById("mainNav").className = "navOpen";
-		
-		document.body.style.overflowY = "hidden";
-		
-		document.getElementById("sectionCover").style.display = "block";
-		
-	}
-	
-}
-
-function closeNav(){
-	
-	if(document.getElementById("mainNav").offsetWidth != 0){
-		
-		document.getElementById("mainNav").className = "navClosed";
-		
-		document.body.style.overflowY = "auto";
-		
-		document.getElementById("sectionCover").style.display = "none";
-		
-	}
-	
-}
-
 function swipeStart(event){
 	
 	tapStartX = event.pageX;
