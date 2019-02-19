909ecbf676ac0f5b054d10881c60c48ff916e7b4
Jak <jaksawesome10@github.com>
2019-02-19 18:07:24 -0500

    notes kinda work

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -13,9 +13,66 @@ function loadFunction(){
 
 window.addEventListener("load", loadFunction());
 
+function allStorage() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        values.push(keys[i]);
+    }
+
+    return values;
+	
+}
+
+function createNewElement(elementTag, elementId, parentElement){
+	
+	var element = document.createElement(elementTag);
+	var t = document.createTextNode("");
+	element.appendChild(t);
+	parentElement.appendChild(element);
+	
+	return element;
+	
+}
+
 function openNav(){
 	
 	if(document.getElementById("mainNav").offsetWidth == 0){
+		
+		//Makes Notes Options
+		
+		document.getElementById("mainNav").innerHTML = "";
+		
+		settingsButton = createNewElement("div", null, document.getElementById("mainNav"));
+		
+		settingsButton.className = "navOption";
+		
+		settingsButton.onclick = function(){openSettings()};
+		
+		settingsButton.innerHTML = "Settings";
+		
+		var notes = allStorage();
+		
+		for(var i = 0; i < notes.length; i++){
+			
+			if(notes[i].substring(0, 5) == "note-"){
+				
+				newNote = createNewElement("div", null, document.getElementById("mainNav"));
+				
+				newNote.className = "navOption";
+				
+				newNote.innerText = notes[i].substring(5);
+				
+				newNote.onclick = function(){openNote(this)};
+				
+			}
+			
+		}
+		
+		//Opens Nav
 		
 		document.getElementById("mainNav").className = "navOpen";
 		
@@ -82,4 +139,88 @@ function openSettings(){
 	
 }
 
+function saveNote(){
+	
+	noteTitle = document.getElementById("noteTitleInput").value;
+	
+	noteContents = document.getElementById("mainTextbox").value;
+	
+	//Empty Contents - Does not work
+	
+	if(noteContents == ""){
+		
+		document.getElementById("saveAlert").innerText = "Please enter notes.";
+		
+		document.getElementById("saveAlert").style.color = "red";
+		
+		$(document).ready(async function(){
+			
+			if(getComputedStyle(document.getElementById("saveAlert"), null).display == "none"){
+				
+				$("#saveAlert").slideDown(500);
+				
+				await sleep(2000);
+				
+				$("#saveAlert").slideUp(500);
+				
+			}
+			
+		});
+		
+	}
+	
+	//Has contents
+	
+	else{
+		
+		if(noteTitle == ""){
+			
+			if(noteContents.length >= 15){
+				
+				noteTitle = noteContents.substring(0, 14);
+				
+			}
+			
+			else{
+				
+				noteTitle = noteContents;
+				
+			}
+			
+		}
+		
+		localStorage.setItem("note-" + noteTitle, noteContents);
+		
+		document.getElementById("saveAlert").innerText = "Note Saved!";
+		
+		document.getElementById("saveAlert").style.color = "black";
+		
+		$(document).ready(async function(){
+			
+			if(getComputedStyle(document.getElementById("saveAlert"), null).display == "none"){
+				
+				$("#saveAlert").slideDown(500);
+				
+				await sleep(2000);
+				
+				$("#saveAlert").slideUp(500);
+				
+			}
+			
+		});
+		
+	}
+	
+}
 
+function openNote(option){
+	
+	console.log(option)
+	
+	document.getElementById("noteTitleInput").value = option.innerText;
+	
+	document.getElementById("mainTextbox").value = localStorage.getItem("note-" + option.innerText);
+	
+	closeNav();
+	
+}
