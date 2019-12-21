46a160219d7d35c2271ad438eccd9abcf09c7638
Jak <jaksawesome10@github.com>
2019-12-20 22:20:00 -0500

    got the notes and lists seperated

--- NewFreeXT/FreeXT/App.js
+++ NewFreeXT/FreeXT/App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
 import Constants from 'expo-constants';
 import { FloatingAction } from "react-native-floating-action";
-import { Header } from "react-native-elements";
+import { Header, Icon } from "react-native-elements";
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from "react-navigation-stack";
 import NoteScreen from "./Note.js";
+import {AsyncStorage} from 'react-native';
 
 //For add note button
 
@@ -34,9 +35,9 @@ class HomeScreen extends React.Component {
         leftComponent={{ icon: 'menu', color: '#fff', underlayColor: 'rgba(52, 52, 52, 0)', size:30, onPress: ()=>{alert("This should open the drawer");} }}
         centerComponent={{ text: 'FreeXT', style: { color: '#fff', fontSize: 25 } }}
         />
-        <NotePreview title="Groceries" navigate={navigate}/>
-        <NotePreview title="Stuff" navigate={navigate}/>
-        <NotePreview title="Other Stuff" navigate={navigate}/>
+        <NotePreview type="list" title="Groceries" navigate={navigate}/>
+        <NotePreview type="regular" title="Stuff" navigate={navigate}/>
+        <NotePreview type="regular" title="Other Stuff" navigate={navigate}/>
         <FloatingAction actions={actions}/>
       </View>
     );
@@ -46,8 +47,11 @@ class HomeScreen extends React.Component {
 class NotePreview extends React.Component{
   render(){
     return(
-      <Touchable style={styles.note} background={Touchable.Ripple('grey')} onPress={()=>{this.props.navigate("Note", {title: this.props.title})}}>
-        <Text style={styles.noteTitle}>{this.props.title}</Text>
+      <Touchable style={styles.note} background={Touchable.Ripple('grey')} onPress={()=>{this.props.navigate("Note", {title: this.props.title, type: this.props.type})}}>
+        <View style={styles.noteContainer}>
+          <Text style={styles.noteTitle}>{this.props.title}</Text>
+          <Icon style={styles.noteIcon} name={this.props.type=="regular"?"note":"list"} />
+        </View>
       </Touchable>
     );
   }
@@ -69,8 +73,15 @@ const styles = StyleSheet.create({
     borderBottomColor: '#EDEDED',
     alignSelf: 'stretch',
   },
+  noteContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   noteTitle: {
     fontSize: 15,
+  },
+  noteIcon: {
+
   },
 });
 
--- NewFreeXT/FreeXT/Note.js
+++ NewFreeXT/FreeXT/Note.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet, Dimensions, ScrollView } from "react-native";
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import { createStackNavigator } from "react-navigation-stack";
+import { createStackNavigator, Header } from "react-navigation-stack";
+import Constants from 'expo-constants';
 
 export default class NoteScreen extends React.Component{
   static navigationOptions = ({ navigation }) => {
@@ -11,10 +12,30 @@ export default class NoteScreen extends React.Component{
   };
   render(){
     const { navigation } = this.props;
-    return(
-      <View>
-        <Text>This notes page : {navigation.getParam("title")}</Text>
-      </View>
-    );
+    if(navigation.getParam("type") == "regular"){
+      return(
+        <View>
+          <ScrollView><TextInput style={styles.textbox} multiline={true} placeholder="Type Here" autofocus={true}/></ScrollView>
+        </View>
+      );
+    }
+    else{
+      return (
+        <View>
+          <Text>This is a list page</Text>
+        </View>
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  textbox: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height-Header.HEIGHT,
+    backgroundColor: "#e8e8e8",
+    textAlignVertical: "top",
+    padding: 10,
+    fontSize: 18,
+  },
+});
